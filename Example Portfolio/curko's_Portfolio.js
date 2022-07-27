90806a4e02bc95b44b51d61fb72c46ad0efb05e5
Tsvetoslav Nikolov <cyrkonikoloww@gmail.com>
2022-07-27 20:20:00 +0300

    content arrangement

---
+++ Example Portfolio/curko's_Portfolio.js
@@ -0,0 +1,22 @@
+var typingEffect = new Typed(".multiText", {
+    strings: ["Tsvetoslav Nikolov", "Web Developer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+})
+
+var typingEffect = new Typed(".pText", {
+    strings: ["Hello! I am Tsvetoslav Nikolov and I am a front-end web developer.I am 19 years old from Vratsa and I study at Plovdiv University majoring in Informatics."],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+        
+})
+
+var typingEffect = new Typed(".fText", {
+    strings: ["Find me on social networks: "],
+        typeSpeed: 40,
+        backSpeed: 60,
+        loop: true
+})
