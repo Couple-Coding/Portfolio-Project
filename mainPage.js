17085438c11e0f63a054ad9b7d503781655e8df9
Tsvetoslav Nikolov <cyrkonikoloww@gmail.com>
2022-07-27 22:19:23 +0300

    new localhost

--- mainPage.js
+++ mainPage.js
@@ -1,11 +1,11 @@
 function btn1() {
     document.getElementsByClassName(".btn1");
-    location.href= "http://localhost:5500/Example%20Portfolio/portfolio.html"
+    location.href= "http://127.0.0.1:5500/Example%20Portfolio/Curko%27s%20Portfolio.html"
   }
 
   function btn2() {
     document.getElementsByClassName(".btn2");
-    location.href= "http://localhost:5500/Example%20Portfolio/portfolio2.html"
+    location.href= "http://127.0.0.1:5500/Example%20Portfolio/Mila%27s%20Portfolio.html"
 
   }
   
