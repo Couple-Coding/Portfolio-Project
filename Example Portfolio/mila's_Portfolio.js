6c825146ca24246cf9a4f94d5372a936681baea8
Mila Yonkova <miladimitrova11@gmail.com>
2022-07-27 23:53:45 +0300

    alert

--- Example Portfolio/mila's_Portfolio.js
+++ Example Portfolio/mila's_Portfolio.js
@@ -23,3 +23,16 @@ var typingEffect = new Typed(".fText", {
     backSpeed: 60,
     loop: true
 })
+
+function checkform()
+{
+  if (document.getElementById("mail").value == "")
+{
+    // something is wrong
+    alert('There is a problem with the first field');
+    return false;
+}
+
+return true;
+}
+
