1e9e4cd76d43497d1a87bebac25f6cc2de8dcf4a
Mila Yonkova <miladimitrova11@gmail.com>
2022-07-28 20:08:43 +0300

    aaaaa

--- Example Portfolio/mila's_Portfolio.js
+++ Example Portfolio/mila's_Portfolio.js
@@ -52,3 +52,16 @@ function toggle() {
     var popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
+
+
+        var menuList = document.getElementById("menuList");
+        menuList.style.maxHeight = "0px";
+        function togglemenu(){
+            if(menuList.style.maxHeight == "0px"){
+                menuList.style.maxHeight = "130px";
+            }else{
+                menuList.style.maxHeight = "0px";
+            }
+
+        }
+
